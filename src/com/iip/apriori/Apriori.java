cc185877f0cfa5671b7713f9b3cf4ff16ea0a3c3
lzy <18646083985@163.com>
2015-05-28 21:28:24 +0800

    init Apriori Alogrithm Project

---
+++ src/com/iip/apriori/Apriori.java
@@ -0,0 +1,23 @@
+package com.iip.apriori;
+
+import java.util.List;
+
+import com.iip.tool.ReadData;
+
+public class Apriori {
+	
+	public void run(String fileName) {
+		List<String[]> dataList = ReadData.readCommaFile(fileName);
+		
+		
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
